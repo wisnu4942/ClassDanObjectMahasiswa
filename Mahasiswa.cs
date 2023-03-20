71a5bb92bebb2accb190861d667c6f5b6cc96fe2
wisnu4942 <127730366+wisnu4942@users.noreply.github.com>
2023-03-20 18:17:12 +0700

    Add files via upload

---
+++ Mahasiswa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+        public float Ipk { get; set; }
+        public void Data()
+        {
+            Console.WriteLine("NAMA: {0}", Nama);
+            Console.WriteLine("NIM: {0}", Nim);
+            Console.WriteLine("IPK: {0}", Ipk);
+        }
+        public void Registrasi()
+        {
+            Console.WriteLine("\nSudah melakukan registrasi");
+        }
+        public void IsiKrs()
+        {
+            Console.WriteLine("Sedang mengisi Krs\n");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace LatihanClassDanObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            saya.Nama = "Wisnu";
+            saya.Nim = "22.11.4942";
+            saya.Ipk = 3.90f;
+
+            kamu.Nama = "Rosa";
+            kamu.Nim = "22.11.2494";
+            kamu.Ipk = 2.90f;
+
+            saya.Data();
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Data();
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
